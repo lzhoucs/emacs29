632a23395a8a188fd81863d2e2d040bd8e8a248a
Liang Zhou <lzhoucs@gmail.com>
2023-01-18 02:19:51 -0600

    better.

--- early-init.el
+++ early-init.el
@@ -1,11 +1,13 @@
 (setq inhibit-startup-screen t)
 
-(setq package-enable-at-startup nil)
-
 (setq native-comp-deferred-compilation t)
 (setq native-comp-async-report-warnings-errors nil)
 
 (setq treesit-extra-load-path '("/home/lzhou/code/oss/tree-sitter-module/dist"))
 
+( setq custom-file (locate-user-emacs-file "custom-vars.el") )
+
+(global-auto-revert-mode 1)
+( setq use-dialog-box nil )
 (provide 'early-init)
 ;;; early-init.el ends here
--- init.el
+++ init.el
@@ -3,13 +3,12 @@
 (tooltip-mode    -1)
 (menu-bar-mode   -1)
 
-;; (require 'package)
-;; (setq package-enable-at-startup nil)
-;; (setq package-archives '(("org"   . "http://orgmode.org/elpa/")
-;;                          ("gnu"   . "http://elpa.gnu.org/packages/")
-;;                          ("melpa" . "https://melpa.org/packages/")))
+(require 'package)
+(setq package-archives '(("org"   . "http://orgmode.org/elpa/")
+                         ("gnu"   . "http://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
 
-;; (package-initialize)
+(package-initialize)
 
 (require 'use-package)
 (require 'use-package-ensure)
@@ -26,16 +25,3 @@
   (setq evil-want-keybinding nil)
   :config
   (evil-mode 1))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(zenburn-theme)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
