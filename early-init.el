d39e0dd07015d07c31cfb5cd8130f7726dffc04b
Liang Zhou <lzhoucs@gmail.com>
2023-01-31 00:23:00 -0600

    Save for now.

--- early-init.el
+++ early-init.el
@@ -1,13 +1,39 @@
+(setq gc-cons-threshold (* 50 1000 1000))
+(menu-bar-mode   -1)
+(scroll-bar-mode -1)
+(tool-bar-mode   -1)
+(tooltip-mode    -1)
+
 (setq inhibit-startup-screen t)
 
+;; Make native compilation happens asynchronously
 (setq native-comp-deferred-compilation t)
 (setq native-comp-async-report-warnings-errors nil)
+(setq initial-scratch-message nil)
+(setq inhibit-compacting-font-caches t)
+
+;; (setq completion-auto-select 'second-tab)
+
+(define-key global-map (kbd "C-<tab>") 'other-window)
+(define-key global-map (kbd "C-=") 'text-scale-increase)
+(define-key global-map (kbd "C--") 'text-scale-decrease)
 
 (setq treesit-extra-load-path '("/home/lzhou/code/oss/tree-sitter-module/dist"))
+
 
 ( setq custom-file (locate-user-emacs-file "custom-vars.el") )
 
 (global-auto-revert-mode 1)
 ( setq use-dialog-box nil )
+
+(add-hook 'emacs-startup-hook
+  (lambda ()
+    (message "Emacs ready in %s with %d garbage collections."
+      (format "%.2f seconds"
+        (float-time
+          (time-subtract
+            after-init-time before-init-time)))
+      gcs-done)))
+
 (provide 'early-init)
 ;;; early-init.el ends here
--- init.el
+++ init.el
@@ -1,8 +1,3 @@
-(scroll-bar-mode -1)
-(tool-bar-mode   -1)
-(tooltip-mode    -1)
-(menu-bar-mode   -1)
-
 (require 'package)
 (setq package-archives '(("org"   . "http://orgmode.org/elpa/")
                          ("gnu"   . "http://elpa.gnu.org/packages/")
@@ -15,13 +10,220 @@
 (setq use-package-always-ensure t)
 
 ;; Theme
-(use-package zenburn-theme
+(use-package doom-themes
   :config
-  (load-theme 'zenburn t))
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-zenburn t))
+
+;; (load-theme 'modus-vivendi)
+;;
+;; (use-package emacs
+;;   :config
+;;   (require-theme 'modus-themes) ; `require-theme' is ONLY for the built-in Modus themes
+
+;;   ;; Add all your customizations prior to loading the themes
+;;   ;; (setq modus-themes-italic-constructs t
+;;   ;;       modus-themes-bold-constructs nil)
+
+;;   ;; Maybe define some palette overrides, such as by using our presets
+;;   ;; (setq modus-themes-common-palette-overrides
+;;   ;;       modus-themes-preset-overrides-intense)
+
+;;   ;; Load the theme of your choice.
+  ;; (load-theme 'modus-operandi)
+  ;; :bind ("<f5>" . modus-themes-toggle))
+
+
+
 
 ;; Evil mode
-(use-package evil
-  :init
-  (setq evil-want-keybinding nil)
+;; (use-package evil
+;;   :init
+;;   (setq evil-want-keybinding nil)
+;;   :config
+;;   (evil-mode 1))
+
+(defun meow-setup ()
+  (setq meow-cheatsheet-layout meow-cheatsheet-layout-qwerty)
+  (meow-motion-overwrite-define-key
+   '("j" . meow-next)
+   '("k" . meow-prev)
+   '("<escape>" . ignore))
+
+  (meow-leader-define-key
+   ;; SPC j/k will run the original command in MOTION state.
+   '("j" . "H-j")
+   '("k" . "H-k")
+   ;; Use SPC (0-9) for digit arguments.
+   '("1" . meow-digit-argument)
+   '("2" . meow-digit-argument)
+   '("3" . meow-digit-argument)
+   '("4" . meow-digit-argument)
+   '("5" . meow-digit-argument)
+   '("6" . meow-digit-argument)
+   '("7" . meow-digit-argument)
+   '("8" . meow-digit-argument)
+   '("9" . meow-digit-argument)
+   '("0" . meow-digit-argument)
+   '("/" . meow-keypad-describe-key)
+   '("?" . meow-cheatsheet)
+   '(":" . execute-extended-command)
+   '("ab" . split-window-horizontally)
+   )
+
+  (meow-normal-define-key
+   '("0" . meow-expand-0)
+   '("9" . meow-expand-9)
+   '("8" . meow-expand-8)
+   '("7" . meow-expand-7)
+   '("6" . meow-expand-6)
+   '("5" . meow-expand-5)
+   '("4" . meow-expand-4)
+   '("3" . meow-expand-3)
+   '("2" . meow-expand-2)
+   '("1" . meow-expand-1)
+   '("-" . negative-argument)
+   '(";" . meow-reverse)
+   '("," . meow-inner-of-thing)
+   '("." . meow-bounds-of-thing)
+   '("[" . meow-beginning-of-thing)
+   '("]" . meow-end-of-thing)
+   '("a" . meow-append)
+   '("A" . meow-open-below)
+   '("b" . meow-back-word)
+   '("B" . meow-back-symbol)
+   '("c" . meow-change)
+   '("d" . meow-delete)
+   '("D" . meow-backward-delete)
+   '("e" . meow-next-word)
+   '("E" . meow-next-symbol)
+   '("f" . meow-find)
+   '("g" . meow-cancel-selection)
+   '("G" . meow-grab)
+   '("h" . meow-left)
+   '("H" . meow-left-expand)
+   '("i" . meow-insert)
+   '("I" . meow-open-above)
+   '("j" . meow-next)
+   '("J" . meow-next-expand)
+   '("k" . meow-prev)
+   '("K" . meow-prev-expand)
+   '("l" . meow-right)
+   '("L" . meow-right-expand)
+   '("m" . meow-join)
+   '("n" . meow-search)
+   '("o" . meow-block)
+   '("O" . meow-to-block)
+   '("p" . meow-yank)
+   '("q" . meow-quit)
+   '("Q" . meow-goto-line)
+   '("r" . meow-replace)
+   '("R" . meow-swap-grab)
+   '("s" . meow-kill)
+   '("t" . meow-till)
+   '("u" . meow-undo)
+   '("U" . meow-undo-in-selection)
+   '("v" . meow-visit)
+   '("w" . meow-mark-word)
+   '("W" . meow-mark-symbol)
+   '("x" . meow-line)
+   '("X" . meow-goto-line)
+   '("y" . meow-save)
+   '("Y" . meow-sync-grab)
+   '("z" . meow-pop-selection)
+   '("'" . repeat)
+   '("<escape>" . ignore)))
+
+(use-package meow
   :config
-  (evil-mode 1))
+  (require 'meow)
+  (meow-setup)
+  (meow-global-mode 1)
+  )
+
+;; Enable vertico
+(use-package vertico
+  :bind (:map vertico-map
+         ("C-l" . vertico-insert))
+  :init
+  (vertico-mode)
+  (vertico-reverse-mode)
+
+  ;; Different scroll margin
+  ;; (setq vertico-scroll-margin 0)
+
+  ;; Show more candidates
+  (setq vertico-count 20)
+
+  ;; Grow and shrink the Vertico minibuffer
+  (setq vertico-resize t)
+
+  ;; Optionally enable cycling for `vertico-next' and `vertico-previous'.
+  (setq vertico-cycle t)
+  )
+
+;; Persist history over Emacs restarts. Vertico sorts by history position.
+(use-package savehist
+  :init
+  (savehist-mode))
+
+;; Enable rich annotations using the Marginalia package
+(use-package marginalia
+  :after vertico
+  ;; :bind (:map minibuffer-local-map
+  ;;        ("M-A" . marginalia-cycle))
+
+  ;; The :init configuration is always executed (Not lazy!)
+  :init
+
+  ;; Must be in the :init section of use-package such that the mode gets
+  ;; enabled right away. Note that this forces loading the package.
+  (marginalia-mode))
+
+(use-package orderless
+  :init
+  ;; Configure a custom style dispatcher (see the Consult wiki)
+  ;; (setq orderless-style-dispatchers '(+orderless-dispatch)
+  ;;       orderless-component-separator #'orderless-escapable-split-on-space)
+  (setq completion-styles '(orderless basic)
+        completion-category-defaults nil
+        completion-category-overrides '((file (styles partial-completion)))))
+
+(use-package company
+  :config
+  (global-company-mode 1)
+  )
+;; A few more useful configurations...
+(use-package emacs
+  :init
+  ;; Add prompt indicator to `completing-read-multiple'.
+  ;; We display [CRM<separator>], e.g., [CRM,] if the separator is a comma.
+  (defun crm-indicator (args)
+    (cons (format "[CRM%s] %s"
+                  (replace-regexp-in-string
+                   "\\`\\[.*?]\\*\\|\\[.*?]\\*\\'" ""
+                   crm-separator)
+                  (car args))
+          (cdr args)))
+  (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
+
+  ;; Do not allow the cursor in the minibuffer prompt
+  (setq minibuffer-prompt-properties
+        '(read-only t cursor-intangible t face minibuffer-prompt))
+  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
+
+  ;; Emacs 28: Hide commands in M-x which do not work in the current mode.
+  ;; Vertico commands are hidden in normal buffers.
+  ;; (setq read-extended-command-predicate
+  ;;       #'command-completion-default-include-p)
+
+  ;; Enable recursive minibuffers
+  (setq enable-recursive-minibuffers t))
+
+;; (use-package which-key
+;;   :init
+;;   (setq which-key-idle-delay .4)
+;;   :config
+;;   (which-key-mode)
+;;   )
